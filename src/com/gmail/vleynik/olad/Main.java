461745f3e2099f9da85b20c5bdee98d3d1e53960
Vladyslav <olad.vleynik@gmail.com>
2021-11-04 01:03:40 +0200

    message

--- src/com/gmail/vleynik/olad/Main.java
+++ src/com/gmail/vleynik/olad/Main.java
@@ -1,18 +1,12 @@
 package com.gmail.vleynik.olad;
 
 import javafx.application.Application;
-import javafx.application.Platform;
-import javafx.event.EventHandler;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
-import javafx.stage.WindowEvent;
 
 import java.io.File;
-import java.util.Objects;
 
 public class Main extends Application {
 
@@ -20,9 +14,9 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-
         videoFile = new File(LastVideo.load());
         MediaViewSetup mediaViewSetup = new MediaViewSetup(videoFile, 640,480);
+
 
         Scene scene = new Scene(new Pane(mediaViewSetup.getMediaView()));
         primaryStage.setScene(scene);
@@ -33,27 +27,8 @@ public class Main extends Application {
         primaryStage.show();
 
 
-        Stage controlPanel = new Stage();
-        FXMLLoader loader = new FXMLLoader(Objects.requireNonNull(getClass().getResource("ControlPanel.fxml")));
-        Parent root = loader.load();
-        ControlPanelController controlPanelController = loader.getController();
-        controlPanelController.setPrimaryStage(primaryStage);
-        controlPanelController.setMediaView(mediaViewSetup.getMediaView());
-        controlPanelController.setMediaPlayer(mediaViewSetup.getMediaPlayer());
-        controlPanelController.setVideoName(videoFile.getName());
-        mediaViewSetup.setCpController(controlPanelController);
-        controlPanel.setScene(new Scene(root));
-        controlPanel.setAlwaysOnTop(true);
-        controlPanel.setX(0);
-        controlPanel.setY(485);
-        controlPanel.setResizable(false);
-        controlPanel.initStyle(StageStyle.UTILITY);
-        controlPanel.setOnCloseRequest(new EventHandler<WindowEvent>() {
-            public void handle(WindowEvent we) {
-                primaryStage.close();
-            }
-        });
-        controlPanel.show();
+        ControlPanelStageSetup cpStageSetup = new ControlPanelStageSetup ("ControlPanel.fxml", primaryStage, mediaViewSetup, videoFile);
+        cpStageSetup.getControlPanel().show();
     }
 
     public static void main(String[] args) {
